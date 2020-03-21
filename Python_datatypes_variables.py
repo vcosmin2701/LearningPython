aa9488682317540e1c5eed022f193851ee95fdb7
vcosmin2701 <38065643+vcosmin2701@users.noreply.github.com>
2020-03-21 21:02:14 +0200

    Add files via upload
    Python data types and variables.

---
+++ Python_datatypes_variables.py
@@ -0,0 +1,21 @@
+# Python Data Types and Variables
+
+"""
+- The data types of an item defines the type and range
+  of values that item can have.
+- Python provides three main data type : nunbers , strings, booleans.
+
+Variables :
+- A variable is simply a name to which a value can be assigned (like a box)
+- Variables are mutable . Hence, the value of a variable can always be updated
+  and replaced 
+
+Naming convention:
+- the name can start with an upper or lower case alphabet.
+- a number can appear in the name , but not at the beginning.
+- underscore (_) can appear anywhere in the name.
+- spaces are not allowed.
+- the variable name should be something meaningful that describes
+  the value it holds
+
+"""
